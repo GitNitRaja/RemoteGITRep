3349d4f8d0cd02aeda9a935d1bb970e7f0330309
raviDev1 <Dev1@gmail.com>
2017-04-30 09:49:58 +0530

    Delete change->dev1

--- TestProj1/src/MakeUpperCase.java
+++
@@ -1,7 +0,0 @@
-
-public class MakeUpperCase {
-
-	 public String ConvertToUpperCase(String content){
-		 return content.toUpperCase();
-	 }
-}
--- TestProj1/src/SampleApp.java
+++ TestProj1/src/SampleApp.java
@@ -2,11 +2,10 @@
 public class SampleApp {
 
 	public static void main(String[] args) {
-		MakeUpperCase muc=null;
+
 		System.out.println("welcome to GIT1");
 		System.out.println("welcome to GIT2");
 		System.out.println("welcome to GIT3-->Dev2");
-		muc=new MakeUpperCase();
-	   System.out.println(muc.ConvertToUpperCase("BahuBhali"));
+
 	}//main
 }//class
