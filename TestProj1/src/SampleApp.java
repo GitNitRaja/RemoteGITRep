7fbf9623917f8811d75b8b752e8aed2dd65a49c2
raviDev1 <Dev1@gmail.com>
2017-04-30 09:33:54 +0530

    Chanage1---Dev2

--- TestProj1/src/SampleApp.java
+++ TestProj1/src/SampleApp.java
@@ -4,6 +4,7 @@ public class SampleApp {
 	public static void main(String[] args) {
 		System.out.println("welcome to GIT1");
 		System.out.println("welcome to GIT2");
+		System.out.println("welcome to GIT3-->Dev2");
 	}
 
 }
